195b90bec8f135359d8c5cde2c5e49c98729e6f1
Medowuxa <veron.spb@mail.ru>
2024-11-23 01:09:53 +0300

    a

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,28 @@
 import kotlin.system.exitProcess
 
-fun main(){
+fun main() {
     val pizzaPeter = PizzaPeter(
-        175.0,241.5,
-        167.5,215.0
+        175.0, 241.5,
+        167.5, 215.0
     )
     val pizzaMoscow = PizzaMoscow(
-        215.0,250.5,
-        180.5,240.0
+        215.0, 250.5,
+        180.5, 240.0
+    )
+    val pizzaOmsk = PizzaOmsk(
+        150.0, 260.5,
+        170.5, 230.0
     )
     var currentPizzaCity: PizzaCity
 
     while (true) {
         println("Добрый день! Выберите город")
-        println("1. Москва\n2.Санкт-Петербург\n")
+        println("1.Москва\n2.Санкт-Петербург\n3.Омск\n")
 
         currentPizzaCity = when (readln()) {
             "1" -> pizzaMoscow
             "2" -> pizzaPeter
+            "3" -> pizzaOmsk
             "0" -> break
             else -> {
                 println("ERROR")
@@ -26,7 +31,7 @@ fun main(){
         }
 
         println("Выберите пиццу:")
-        println("1. Неаполтанская пицца\n2.Римская пицца\n3. Сицилийская пицца\n4.Тирольская пицца\n\n0.Показать статистику")
+        println("1.Неаполтанская пицца\n2.Римская пицца\n3.Сицилийская пицца\n4.Тирольская пицца\n\n0.Показать статистику")
         selectPizza(currentPizzaCity)
 
     }
@@ -62,9 +67,14 @@ private fun selectPizza(currentPizzaCity: PizzaCity) {
     }
 }
 
-fun selectAddService(currentPizzaCity: PizzaCity){
-    when (currentPizzaCity) {
-        is CheckPhoto -> currentPizzaCity.showCheckPhoto()
-        is Drink -> currentPizzaCity.drinkSale()
+fun selectAddService(currentPizzaCity: PizzaCity) {
+    if (currentPizzaCity is Drink) {
+        currentPizzaCity.drinkSale() // Спрашивает о покупке кофе
+    }
+    if (currentPizzaCity is CheckPhoto) {
+        currentPizzaCity.showCheckPhoto() // Спрашивает о показе чека
+    }
+    if (currentPizzaCity is Sauce) {
+        currentPizzaCity.sauceSale()
     }
 }
